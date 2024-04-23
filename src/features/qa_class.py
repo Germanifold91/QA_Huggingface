6943114c41a6c872bcada383376a35a7e525116a
Germanifold <germanifold@outlook.com>
2024-04-22 20:09:10 -0500

    Construction of document answering methods on DocumentAssistant class. This methods answer a question from the most relevant document found by the previous methods.

--- src/features/qa_class.py
+++ src/features/qa_class.py
@@ -1,14 +1,16 @@
 from transformers import AutoTokenizer, AutoModel, pipeline
 from torch import Tensor
 from datasets import Dataset
-from typing import List, Any
+from typing import List, Any, Tuple
 import torch
+import os
 
 
 class DocumentAssistant:
     def __init__(self, model_ckpt: str = "sentence-transformers/multi-qa-mpnet-base-dot-v1"):
         self.tokenizer = AutoTokenizer.from_pretrained(model_ckpt)
         self.model = AutoModel.from_pretrained(model_ckpt)
+
     def mean_pooling(self, model_output: Tensor, attention_mask: Tensor) -> Tensor:
         """
 
@@ -30,6 +32,7 @@ class DocumentAssistant:
         )
         with torch.no_grad():
             model_output = self.model(**encoded_input)
+
         return self.mean_pooling(model_output, encoded_input["attention_mask"])
     
     def text_search(self, master_dataset: Dataset, question: str, num_sources: int = 1) -> List[Any]:
@@ -48,3 +51,34 @@ class DocumentAssistant:
         )
 
         return samples["TEXT_FILE_PATH"]
+    
+    def question_answering(self, question: str, paths_list: List[str]) -> Tuple[str, List[str]]:
+        """
+
+        """
+        context = ""
+        file_names = []
+
+        for path in paths_list:
+            with open(path, 'r', encoding='utf-8') as file:
+                context += "\n" + file.read()
+            file_names.append(os.path.basename(path))
+
+        qa_pipeline = pipeline("question-answering", model = "deepset/roberta-base-squad2")
+        
+        result = qa_pipeline(question=question, context=context, max_answer_len=100)
+        
+        return result['answer'], file_names
+    
+    def answer_pipeline(self, master_dataset: Dataset, question: str) -> Tuple[str, List[str]]:
+        """
+
+        """
+        texts_paths = self.text_search(master_dataset=master_dataset, question=question)
+        
+        answer, files_names = self.question_answering(question=question, paths_list=texts_paths)
+
+        # Replace '.txt' with '.md' in the file names
+        md_files_names = [fname.replace('.txt', '.md') for fname in files_names]
+        
+        return answer, md_files_names
