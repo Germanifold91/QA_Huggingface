7c019a59473e5c4d149aa9ca4bf85ccdefd5b990
Germanifold <germanifold@outlook.com>
2024-04-24 11:03:52 -0500

    Exclusion of title tokens process and elimination of utils folder given its lack of use in the project

--- src/etl/data_processing.py
+++ src/etl/data_processing.py
@@ -1,4 +1,3 @@
-from src.utils.text_utils import clean_text
 from typing import Dict, Any
 from bs4 import BeautifulSoup
 from datasets import Dataset
@@ -10,14 +9,12 @@ import yaml
 def create_file_table(folder_path: str, 
                       destination_path: str = None, 
                       save: bool = False) -> pd.DataFrame:
-
     """
     """
     if not os.path.isdir(folder_path):
         raise ValueError(f"The provided folder path does not exist: {folder_path}")
 
     file_paths = []
-    tokens_list = []
 
     for root, dirs, files in os.walk(folder_path):
         for file in files:
@@ -25,17 +22,11 @@ def create_file_table(folder_path: str,
                 try:
                     full_path = os.path.join(root, file)
                     file_paths.append(full_path)
-
-                    # Extract and clean tokens from the filename
-                    file_name_without_ext = os.path.splitext(file)[0]
-                    cleaned_tokens = clean_text(file_name_without_ext)
-                    tokens_list.append(cleaned_tokens)
                 except Exception as e:
                     print(f"Error processing file {file}: {e}")
 
     file_table = pd.DataFrame({
-        'FILE_PATH': file_paths,
-        'TITLE_TOKENS': tokens_list  
+        'FILE_PATH': file_paths
     })
     
     if save:
